e1282be9c1301ff691f23015376a00133112569e
Mario David Pichardo Vasquez <mariopv90@gmail.com>
2019-04-09 10:10:29 -0400

    Creacion de los controladores

--- Api-Adm-Pro/Api-Adm-Pro/Program.cs
+++ Api-Adm-Pro/Api-Adm-Pro/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Api_Adm_Pro
+namespace ApiAdmPro
 {
     public class Program
     {
